29de1daa6030f4f2c28093b44acb8acb1165fda5
Tharuka <tmttthennakoon@std.foc.sab.ac.lk>
2025-09-27 08:35:58 +0530

    modified main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:waygo/screens/HomePage.dart';
+import 'package:waygo/screens/pages/etickets_page.dart';
+import 'package:waygo/screens/pages/live_location_page.dart';
+import 'package:waygo/screens/pages/news_page.dart';
+import 'package:waygo/screens/pages/schedule_page.dart';
+import 'package:waygo/screens/pages/seat_reservation_page.dart';
+import 'package:waygo/screens/pages/ticket_prices_page.dart';
 import 'package:waygo/screens/signup.dart';
 import 'package:waygo/screens/splash.dart';
-import 'screens/welcome_screen.dart';
+import 'package:waygo/screens/welcome_screen.dart';
+
+// import your new dashboard pages
+
 
 void main() {
   runApp(const MyApp());
@@ -15,14 +24,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Auth Flow',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      title: 'WayGo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue, // later replace with AppColors
+      ),
       initialRoute: '/',
       routes: {
         '/': (context) => const SplashScreen(),
         '/welcome': (context) => const WelcomeScreen(),
         '/signup': (context) => const SignUpScreen(),
-         '/home': (context) => const HomePage(),
+        '/home': (context) => const HomePage(),
+
+        // dashboard routes
+        '/news': (context) => const MainNews(),
+        '/live_location': (context) => const LiveLocation(),
+        '/schedule': (context) => const Schedule(),
+        '/seats': (context) => const SeatReservation(),
+        '/prices': (context) => const TicketPrices(),
+        '/eticket': (context) => const Etickets(),
       },
     );
   }
