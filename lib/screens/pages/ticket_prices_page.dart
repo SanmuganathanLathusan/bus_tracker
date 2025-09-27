6b18281e0104811105fa2d3c2b0072da8dd386ec
Tharuka <tmttthennakoon@std.foc.sab.ac.lk>
2025-09-27 08:38:12 +0530

    make connection with dashboard

--- lib/screens/pages/ticket_prices_page.dart
+++ lib/screens/pages/ticket_prices_page.dart
@@ -1,1 +1,14 @@
-// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+class TicketPrices extends StatelessWidget {
+  const TicketPrices({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("ticket prices")),
+      body: const Center(child: Text("News Page Content Here")),
+    );
+  }
+}
+
