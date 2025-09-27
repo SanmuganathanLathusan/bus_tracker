8bab5d0ef8b9651e3ee73a3be8ecfe62f264c4de
Tharuka <tmttthennakoon@std.foc.sab.ac.lk>
2025-09-27 08:37:25 +0530

    add new page to the dashboard

---
+++ lib/screens/pages/live_location_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LiveLocation extends StatelessWidget {
+  const LiveLocation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("live location")),
+      body: const Center(child: Text("News Page Content Here")),
+    );
+  }
+}
