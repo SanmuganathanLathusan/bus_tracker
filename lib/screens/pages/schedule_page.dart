d0a81a25c4f16043eab9846aa07c56b984b6ef70
Tharuka <tmttthennakoon@std.foc.sab.ac.lk>
2025-09-27 08:38:12 +0530

    make connection with dashboard

--- lib/screens/pages/schedule_page.dart
+++ lib/screens/pages/schedule_page.dart
@@ -1,1 +1,14 @@
-// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+class Schedule extends StatelessWidget {
+  const Schedule({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("schedule")),
+      body: const Center(child: Text("News Page Content Here")),
+    );
+  }
+}
+
--- lib/screens/pages/seat_reservation_page.dart
+++ lib/screens/pages/seat_reservation_page.dart
@@ -1,1 +1,14 @@
-// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+class SeatReservation extends StatelessWidget {
+  const SeatReservation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Seat")),
+      body: const Center(child: Text("News Page Content Here")),
+    );
+  }
+}
+
