6d1f457036de0277996c337b3b55ecda3d343e87
SanmuganathanLathusan <sanmuganthanlathusan@gmail.com>
2025-10-20 21:55:25 +0530

    add contants file

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color accentPrimary = Color(0xFF1565C0); // Deep blue accent
+  static const Color textLight = Colors.white;
+  static const Color textDark = Colors.black87;
+}
