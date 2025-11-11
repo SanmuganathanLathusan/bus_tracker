44d68effe3cf1ddb1c222789035efe2fbb842832
SanmuganathanLathusan <sanmuganathanlathusan@gmail.com>
2025-11-11 13:50:31 +0530

    add       lib/services/ file

---
+++ lib/services/api_service.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const String baseUrl = 'http://10.0.2.2:5000/api';
+
+  // Register user
+  static Future<Map<String, dynamic>> registerUser({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    final url = Uri.parse('$baseUrl/auth/register');
+    try {
+      final response = await http.post(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode({"name": name, "email": email, "password": password}),
+      );
+      final data = jsonDecode(response.body);
+      return {"statusCode": response.statusCode, "data": data};
+    } catch (e) {
+      return {"statusCode": 500, "data": {"message": "Could not connect to server: $e"}};
+    }
+  }
+
+  // Login user
+  static Future<Map<String, dynamic>> loginUser({
+    required String email,
+    required String password,
+  }) async {
+    final url = Uri.parse('$baseUrl/auth/login');
+    try {
+      final response = await http.post(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode({"email": email, "password": password}),
+      );
+      final data = jsonDecode(response.body);
+      return {"statusCode": response.statusCode, "data": data};
+    } catch (e) {
+      return {"statusCode": 500, "data": {"message": "Could not connect to server: $e"}};
+    }
+  }
+
+  // Forgot password
+  static Future<Map<String, dynamic>> forgotPassword({
+    required String email,
+  }) async {
+    final url = Uri.parse('$baseUrl/auth/forgot-password');
+    try {
+      final response = await http.post(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode({"email": email}),
+      );
+      final data = jsonDecode(response.body);
+      return {"statusCode": response.statusCode, "data": data};
+    } catch (e) {
+      return {"statusCode": 500, "data": {"message": "Could not connect to server: $e"}};
+    }
+  }
+}
