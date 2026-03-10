4cbbd2caa9f969f0db59d475153b38172bf8f0e1
SanmuganathanLathusan <sanmuganathanlathusan@gmail.com>
2026-03-10 22:10:37 +0530

    modified file

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:waygo/user_screen/HomePage.dart';
-import 'package:waygo/user_screen/forgotpassword.dart';
-import 'package:waygo/user_screen/passenger_dashboard.dart';
-import 'package:waygo/user_screen/pages/live_location_page.dart';
-import 'package:waygo/user_screen/pages/news_page.dart';
-import 'package:waygo/user_screen/pages/schedule_page.dart';
-import 'package:waygo/user_screen/pages/seat_reserve/seat_reservation_home.dart';
-import 'package:waygo/user_screen/pages/ticket_prices_page.dart';
-import 'package:waygo/user_screen/pages/eticket_booking_page.dart';
-import 'package:waygo/user_screen/signup.dart';
-import 'package:waygo/user_screen/splash.dart';
-import 'package:waygo/user_screen/welcome_screen.dart';
-import 'package:waygo/admin_screen/admin_dashboard.dart';
-import 'package:waygo/driver_screen/driver_dashboard.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 
-// import your new dashboard pages
+import 'package:waygo/user_screen/user_pages.dart';
+import 'package:waygo/user_screen/pages/pages.dart';
+import 'package:waygo/admin_screen/admin_pages.dart';
+import 'package:waygo/driver_screen/driver_pages.dart';
 
 void main() {
   Stripe.publishableKey =
       'pk_test_51SU6ZjF3HPXggBggcZjc2M9KhXNMymVXveo6HhKzRqzMtmzOX1CiLUEwDByUNN3XbZUGfGdhRwebp8rc9cfPoB1400pHxV9q8O';
+
   runApp(const MyApp());
 }
 
@@ -32,22 +22,26 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'WayGo',
       theme: ThemeData(
-        primarySwatch: Colors.blue, // later replace with AppColors
+        primarySwatch: Colors.blue,
       ),
+
       initialRoute: '/',
+
       routes: {
+
+        /// Auth & Intro
         '/': (context) => const SplashScreen(),
         '/welcome': (context) => const WelcomeScreen(),
         '/signup': (context) => const SignUpScreen(),
         '/home': (context) => const HomePage(),
         '/forgot-password': (context) => const ForgotPasswordPage(),
 
-        // Role-based dashboards
+        /// Dashboards
         '/passenger-dashboard': (context) => const PassengerDashboard(),
         '/admin-dashboard': (context) => const AdminDashboard(),
         '/driver-dashboard': (context) => const DriverDashboard(),
 
-        // dashboard routes
+        /// App Features
         '/news': (context) => const MainNews(),
         '/live_location': (context) => const LiveLocationPage(),
         '/schedule': (context) => const SchedulePage(),
